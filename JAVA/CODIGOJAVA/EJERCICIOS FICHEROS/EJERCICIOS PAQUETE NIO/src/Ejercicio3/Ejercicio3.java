ff6bbbd0bbdad7e2785147ccba41dc300a03ca3d
oscarfhdev <oscarfh.dev@gmail.com>
2025-03-26 14:30:45 +0100

    CREATE Ejercicio3 -> task3 done

--- JAVA/CODIGOJAVA/EJERCICIOS FICHEROS/EJERCICIOS PAQUETE NIO/src/Ejercicio3/Ejercicio3.java
+++ JAVA/CODIGOJAVA/EJERCICIOS FICHEROS/EJERCICIOS PAQUETE NIO/src/Ejercicio3/Ejercicio3.java
@@ -1,19 +1,22 @@
-package Ejercicio1;
+package Ejercicio3;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 
-public class Ejercicio1 {
+public class Ejercicio3 {
     public static void main(String[] args) {
         Path path = Paths.get("EJERCICIOS PAQUETE NIO/src/Ejercicio1/prueba.txt"); //le decimos la ruta del archivo
-        List<String> contenido = List.of("Trabajando con el paquete .nio");
         try {
-            Files.write(path, contenido);
-            System.out.println("Datos ingresados en el archivo " + path);
-            System.out.println("Nombre del archivo: " + path.getFileName());
+            List<String> lineas = Files.readAllLines(path, StandardCharsets.UTF_8);
+
+             // Imprime cada línea del archivo por consola
+            for (String linea : lineas){
+                System.out.println(linea);
+            }
         }
         catch (IOException e){
             System.out.println("Error: " + e.getMessage());
